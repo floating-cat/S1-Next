7d5601cc7135b60b5e52de7a9df808ce674a3382
floating-cat <monsoon.cl@gmail.com>
2016-05-03 12:58:24 +0800

    1.x Initialization * Clear out some files to start building 1.x
    Purpose:
    * Rewrite app in order to redesign architecture.
    * Fix some mistakes.
    * Change some code style.
    * TDD.
    
    Some mistakes should be fixed in 1.X:
    * Do not extend `Account` for some models, because it is impossible to implement correct equality method for inheritance if we add new fields (see Effective Java Item 8).
    * Remove unneeded getters for models.
    
    Some code style should be fixed in 1.X:
    * Remove unneeded  blank lines after class/interface declaration.
    * Add blank lines for empty block structure.
    * Do not use Hungarian notation ("m" prefix).

---
+++ app/src/test/java/cl/monsoon/s1next/ExampleUnitTest.java
@@ -0,0 +1,11 @@
+package cl.monsoon.s1next;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+//    @Test
+//    public void addition_isCorrect() throws Exception {
+//        assertEquals(4, 2 + 2);
+//    }
+}
